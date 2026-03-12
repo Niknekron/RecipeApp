f6907eb3c7e438a11d4af0ae251420114d1ea09f
Niknekron <niknekron@gmail.com>
2026-03-12 22:29:33 +0300

    RCA-07-Root-UI-migration. Moved the UI logic to a separate file

--- app/src/main/java/ru/niknekron/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/ru/niknekron/recipecomposeapp/MainActivity.kt
@@ -1,48 +1,16 @@
 package ru.niknekron.recipecomposeapp
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import ru.niknekron.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            setContent {
-                MaterialTheme {
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize()
-                    ) { paddingValues ->
-                        Text(
-                            text = "Recipes app",
-                            modifier = Modifier.padding(paddingValues)
-                        )
-                    }
-                }
-            }
+            RecipesApp()
         }
     }
 }
