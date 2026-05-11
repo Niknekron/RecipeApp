e34288b77f4ff3cb6b65a3b3f20e2e50d8d35e6d
Niknekron <niknekron@gmail.com>
2026-05-11 16:09:06 +0300

    RCA-21-SharedPreferences-Basics. Implemented permanent storage of recipes.

--- app/src/main/java/ru/niknekron/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/ru/niknekron/recipecomposeapp/RecipesApp.kt
@@ -22,9 +22,11 @@ import java.nio.charset.StandardCharsets
 import ru.niknekron.recipecomposeapp.ui.details.RecipeDetailsScreen
 import android.content.Intent
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import kotlinx.coroutines.delay
-import ru.niknekron.recipecomposeapp.DEEP_LINK_SCHEME
-import ru.niknekron.recipecomposeapp.PARAM_RECIPE_ID
 import ru.niknekron.recipecomposeapp.data.repository.RecipesRepositoryStub
 import ru.niknekron.recipecomposeapp.ui.recipes.model.toUiModel
 
@@ -159,10 +161,15 @@ fun RecipesApp(
                         .getRecipeById(recipeId)
                         ?.toUiModel()
 
+
+                    var isFavorite by rememberSaveable {
+                        mutableStateOf(false)
+                    }
                     recipe?.let {
                         RecipeDetailsScreen(
                             recipe = it
                         )
+
                     }
                 }
             }
