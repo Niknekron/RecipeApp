e1a88a4c64f375fa84ecdf06f7dc2b7380dc64b7
Niknekron <niknekron@gmail.com>
2026-07-08 20:53:02 +0300

    CA-38-Introduction-to-Jetpack-Room-and-Setup. Connected Room to the project and created an Entity

---
+++ app/src/main/java/ru/niknekron/recipecomposeapp/data/database/dao/CategoryDao.kt
@@ -0,0 +1,15 @@
+package ru.niknekron.recipecomposeapp.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.niknekron.recipecomposeapp.data.database.entity.CategoryEntity
+
+@Dao
+interface CategoryDao {
+
+    @Query(
+        "SELECT * FROM categories ORDER BY name ASC"
+    )
+    fun getAllCategories(): Flow<List<CategoryEntity>>
+}
