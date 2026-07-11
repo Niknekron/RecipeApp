c0641695066baede916a1c7b152e0113926791ba
Niknekron <niknekron@gmail.com>
2026-07-11 10:35:53 +0300

    CA-39-RecipeEntity-and-TypeConverters. Fixed errors.

--- app/src/main/java/ru/niknekron/recipecomposeapp/data/database/converter/Converters.kt
+++ app/src/main/java/ru/niknekron/recipecomposeapp/data/database/converter/Converters.kt
@@ -5,11 +5,14 @@ import androidx.room.TypeConverter
 class Converters {
     @TypeConverter
     fun fromString(value: String): List<String> {
+        if(value.isEmpty()) {
+            return emptyList()
+        }
         return value.split("|||")
     }
 
     @TypeConverter
     fun fromList(list: List<String>): String {
-        return list.joinToString { "|||" }
+        return list.joinToString(separator = "|||")
     }
 }
